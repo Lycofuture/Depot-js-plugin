be1e5045e4d5bc8b573e4a32aef5fcbe6de3cd26
Lycofuture <hizcmua@163.com>
2023-07-30 16:29:57 +0800

    fix: 修复bug，添加功能

--- Commute.js
+++ Commute.js
@@ -2,7 +2,7 @@
  * @Author: Lycofuture
  * @Date: 2023-06-27 00:02:37
  * @LastEditors: Lycofuture 
- * @LastEditTime: 2023-07-29 10:21:47
+ * @LastEditTime: 2023-07-30 16:28:47
  */
 if (!global.segment) {
   try {
@@ -14,11 +14,11 @@ if (!global.segment) {
   }
 }
 import plugin from '../../lib/plugins/plugin.js'
+import Cfg from '../../lib/config/config.js'
 import YAML from 'yaml'
 import fs from 'node:fs'
-let file = './config/config/group.yaml'
-let data = YAML.parse(fs.readFileSync(file, 'utf8'))
-let cmsg
+const file = './config/config/group.yaml'
+const data = YAML.parse(fs.readFileSync(file, 'utf8'))
 export class Commute extends plugin {
   constructor() {
     super({
@@ -36,63 +36,26 @@ export class Commute extends plugin {
           reg: `^上班$`,
           fnc: 'work',
           permission: 'master'
+        },
+        {
+          reg: `^添加主人$`,
+          fnc: 'dominate',
         }
       ]
     })
   }
   async Down(e) {
+    const commnot = new Commute()
+    console.log(commnot)
     if (e.isGroup) {
       data[e.group_id] = {
-        enable: Commute.name
+        enable: commnot.name
       }
-      let yaml = YAML.stringify(data)
+      const yaml = YAML.stringify(data)
       fs.writeFileSync(file, yaml, 'utf8')
-      console.log(data)
       const response = await fetch('https://v1.hitokoto.cn')
       const yiyandata = await response.json()
-      const yiyan = yiyandata.hitokoto
-        .split('，')
-        .slice(0, 2)
-        .map(sentence => sentence.trim())
-        .join('，')
       await e.reply([`派蒙走了哦，不要想我啦\n${yiyandata.hitokoto}`])
-      let ccs = {
-        app: 'com.tencent.miniapp',
-        desc: '',
-        bizsrc: '',
-        view: 'all',
-        ver: '1.0.0.89',
-        prompt: '终于下班了',
-        appID: '',
-        sourceName: '',
-        actionData: '',
-        actionData_A: '',
-        sourceUrl: '',
-        meta: {
-          all: {
-            buttons: [
-              {
-                action: 'http://qq.com',
-                name: '派蒙走了哦，不要想我啦'
-              }
-            ],
-            jumpUrl: 'https://www.bing.com/th?id=OHR.VillandryGarden_ZH-CN6140359139_1920x1080.jpg',
-            preview: 'http://gchat.qpic.cn/gchatpic_new/0/0-0-95821F3DED8669D9D3AEE862234D4C2E/0',
-            summary: '',
-            title: yiyan
-          }
-        },
-        config: {
-          ctime: 1687741340,
-          forward: true,
-          token: '872f4e7f659deaa4c2fb9fb3d56e5071'
-        },
-        text: '',
-        sourceAd: '',
-        extra: ''
-      }
-      // eslint-disable-next-line no-undef
-      // await e.reply(segment.json(JSON.stringify(ccs)))
     } else {
       e.reply('请在群聊中使用')
     }
@@ -102,56 +65,30 @@ export class Commute extends plugin {
       data[e.group_id] = {
         enable: null
       }
-      let yaml = YAML.stringify(data)
+      const yaml = YAML.stringify(data)
       fs.writeFileSync(file, yaml, 'utf8')
-      console.log(data)
       const response = await fetch('https://v1.hitokoto.cn')
       const yiyandata = await response.json()
-      const yiyan = yiyandata.hitokoto
-        .split('，')
-        .slice(0, 2)
-        .map(sentence => sentence.trim())
-        .join('，')
       await e.reply([`派蒙回来了，嘿嘿\n${yiyandata.hitokoto}`])
-      let ccs = {
-        app: 'com.tencent.miniapp',
-        desc: '',
-        bizsrc: '',
-        view: 'all',
-        ver: '1.0.0.89',
-        prompt: '好生气哦，哼~',
-        appID: '',
-        sourceName: '',
-        actionData: '',
-        actionData_A: '',
-        sourceUrl: '',
-        meta: {
-          all: {
-            buttons: [
-              {
-                action: 'http://qq.com',
-                name: '派蒙回来了，嘿嘿'
-              }
-            ],
-            jumpUrl: 'https://www.bing.com/th?id=OHR.VillandryGarden_ZH-CN6140359139_1920x1080.jpg',
-            preview: 'http://gchat.qpic.cn/gchatpic_new/0/0-0-95821F3DED8669D9D3AEE862234D4C2E/0',
-            summary: '',
-            title: yiyan
-          }
-        },
-        config: {
-          ctime: 1687741340,
-          forward: true,
-          token: '872f4e7f659deaa4c2fb9fb3d56e5071'
-        },
-        text: '',
-        sourceAd: '',
-        extra: ''
-      }
-      // eslint-disable-next-line no-undef
-      // await e.reply(segment.json(JSON.stringify(ccs)))
     } else {
       e.reply('请在群聊中使用')
     }
+  }
+  async dominate(e) {
+    if (Cfg.masterQQ[0] === e.user_id) {
+      this.setContext('dominateadd')
+      await e.reply('请发送要添加的主人账号', false, { at: true })
+    } else {
+      await e.reply(`你不是超级管理员\n当前账号超管: ${e.user_id}`)
+    }
+  }
+  dominateadd() {
+    const file = './config/config/other.yaml'
+    const data = YAML.parse(fs.readFileSync(file, 'utf8'))
+    data.masterQQ.push(Number(this.e.msg))
+    const yaml = YAML.stringify(data)
+    fs.writeFileSync(file, yaml, 'utf8')
+    this.reply('添加成功')
+    this.finish('dominateadd')
   }
 }
